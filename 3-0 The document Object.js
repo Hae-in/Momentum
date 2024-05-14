0210ae5a48ceebfb15b219238770a300594cafdf
Haein <93hilee@naver.com>
2024-05-14 19:40:08 +0900

    #5.0 Intervals (05:37)

--- 3-0 The document Object.js
+++
@@ -1,6 +0,0 @@
-// # 3.0 The document Object
-
-const title = document.getElementById("title");
-//console.log(title);
-console.dir(title); // element를 더 자세하게 보여줌
-console.dir(title.innerText);
---
+++ js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello() {
+  console.log("hello");
+}
+
+// 5초마다 실행
+setInterval(sayHello, 5000);
