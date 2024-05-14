ebbf22fdd040238d19e40eceabc20c6154a610d2
Haein <93hilee@naver.com>
2024-05-14 16:57:06 +0900

    #4.0 Input Values (09:48)

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  console.dir(loginInput);
+  console.log(loginInput.value);
+  console.log("clicked!!");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
