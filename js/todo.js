c6e6e7b13c6f70fd173de2e81d8f066dae923788
Haein <93hilee@naver.com>
2024-05-15 16:23:29 +0900

    #7.4 Loading To Dos part One (11:19)

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,21 @@ const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 const todos = [];
 
+const TODOS_KEY = "todos";
+
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 
 function saveTodos() {
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
   // localStorage에 array를 저장할 수 없음
   // JSON.stringify() : JavaScript Object 나 array 또는 어떤 JavaScript 코드건 간에 String으로 변경
+}
+
+function sayHello(item) {
+  console.log("this is turn of" + item);
 }
 
 function paintTodo(newTodo) {
@@ -40,3 +46,14 @@ function handleTodoSubmit(event) {
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+//JSON.parse("STRING") : string을 JSON.parse 안에 넣으면 배열로 만듦
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+
+  // array에 있는 각각의 item에 대해서 실행
+  // parsedTodos.forEach(sayHello);
+  parsedTodos.forEach((item) => console.log("this is turn of", item)); // arrow function
+}
