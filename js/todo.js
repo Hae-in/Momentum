29bedaf8241bb92137c0d3046f2428c340e168f6
Haein <93hilee@naver.com>
2024-05-15 14:56:34 +0900

    #7.1 Adding ToDos (07:00)

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,22 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  //   console.log(li);
+  todoList.appendChild(li);
+}
+
 function handleTodoSubmit(event) {
   event.preventDefault();
   // input 값 저장하고, input 내용 지우기
   const newTodo = todoInput.value;
   todoInput.value = "";
-  console.log(newTodo);
+  // console.log(newTodo);
+  paintTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
