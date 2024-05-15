580c70c278845e69d86ee575a6b7aea8e26a3aa6
Haein <93hilee@naver.com>
2024-05-15 13:33:14 +0900

    #5.1 Timeouts and Dates (08:46)

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,15 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 // 5초마다 실행
-setInterval(sayHello, 5000);
+// setInterval(sayHello, 5000);
+
+// 5초 뒤 실행
+// setTimeout(sayHello, 5000);
+
+getClock(); // website가 load되자마자 getClock()을 실행
+setInterval(getClock, 1000); // 매초마다 실행
