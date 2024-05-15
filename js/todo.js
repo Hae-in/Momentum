30851a5f0a1ea2b543c501f47b712b54e988fbee
Haein <93hilee@naver.com>
2024-05-15 15:54:01 +0900

    #7.3 Saving To Dos (07:26)

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,17 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
+const todos = [];
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  // localStorage에 array를 저장할 수 없음
+  // JSON.stringify() : JavaScript Object 나 array 또는 어떤 JavaScript 코드건 간에 String으로 변경
 }
 
 function paintTodo(newTodo) {
@@ -27,7 +34,9 @@ function handleTodoSubmit(event) {
   const newTodo = todoInput.value;
   todoInput.value = "";
   // console.log(newTodo);
+  todos.push(newTodo);
   paintTodo(newTodo);
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
